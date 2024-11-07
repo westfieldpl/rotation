30f1ba733023acfcd28c57f80c6ae64ed3f79d20
Eli T. Drumm <eli@eli.td>
2024-11-07 17:25:31 -0500

    Update createuser.ps1

--- createuser.ps1
+++ createuser.ps1
@@ -1,2 +1,2 @@
-New-LocalUser -Name "User" -NoPassword -FullName "User" -Description "WWPL Public Access Account" -UserMayNotChangePassword
+New-LocalUser -Name "User" -NoPassword -FullName "User" -Description "WWPL Public Access Account" -UserMayNotChangePassword -PasswordNeverExpires
 Add-LocalGroupMember -Group "Users" -Member "User"
