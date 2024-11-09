b6523e479e9230fbb6ccf7cb7b156f97d76f337a
Eli T. Drumm <eli@eli.td>
2024-11-09 10:09:21 -0500

    Create remove365.ps1

---
+++ remove365.ps1
@@ -0,0 +1,10 @@
+winget uninstall "Microsoft 365 - en-us"
+winget uninstall "Microsoft 365 - es-es"
+winget uninstall "Microsoft 365 - fr-fr"
+winget uninstall "Microsoft 365 - pt-br"
+
+winget uninstall "Microsoft OneNote - en-us"
+winget uninstall "Microsoft OneNote - es-es"
+winget uninstall "Microsoft OneNote - fr-fr"
+winget uninstall "Microsoft OneNote - pt-br"
+
